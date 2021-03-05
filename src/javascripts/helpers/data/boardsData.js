a52fd1fc5f1afec405bf343cfcfa154134f294c8
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-03-05 17:19:41 -0600

    added some functions so that you can delete all associated pins when you delete a board

--- src/javascripts/helpers/data/boardsData.js
+++ src/javascripts/helpers/data/boardsData.js
@@ -3,16 +3,23 @@ import axios from 'axios';
 import firebaseConfig from '../apiKeys';
 
 const dbUrl = firebaseConfig.databaseURL;
-
+// GET BOARDS
 const getBoards = (uid) => new Promise((resolve, reject) => {
   axios.get(`${dbUrl}/boards.json?orderBy="uid"&equalTo="${uid}"`)
     .then((response) => resolve(Object.values(response.data)))
     .catch((error) => reject(error));
 });
+// GET SINGLE BOARD
 const getSingleBoard = (firebaseKey) => new Promise((resolve, reject) => {
   axios.get(`${dbUrl}/boards/${firebaseKey}.json`)
     .then((response) => resolve(response.data))
     .catch((error) => reject(error));
 });
+// DELETE BOARD
+const deleteBoard = (firebaseKey, uid) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/boards/${firebaseKey}.json`)
+    .then(() => getBoards(uid).then((response) => resolve(response)))
+    .catch((error) => reject(error));
+});
 
-export { getBoards, getSingleBoard };
+export { getBoards, getSingleBoard, deleteBoard };
