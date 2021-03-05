c3688c969ee56d5d16c7276383a96429f86a9987
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-03-05 16:10:11 -0600

    added functional delete buttons to pins so user can delete individual pins

--- src/javascripts/helpers/data/pinsData.js
+++ src/javascripts/helpers/data/pinsData.js
@@ -2,10 +2,17 @@ import axios from 'axios';
 import firebaseConfig from '../apiKeys';
 
 const dbUrl = firebaseConfig.databaseURL;
+// GET PINS
 const getPins = (boardId) => new Promise((resolve, reject) => {
   axios.get(`${dbUrl}/pins.json?orderBy="board_ID"&equalTo="${boardId}"`)
     .then((response) => resolve(Object.values(response.data)))
     .catch((error) => reject(error));
 });
+// DELETE PINS
+const deletePins = (pinId, boardId) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/pins/${pinId}.json`)
+    .then(() => getPins(boardId).then((array) => resolve(array)))
+    .catch((error) => reject(error));
+});
 
-export default getPins;
+export { deletePins, getPins };
--- src/javascripts/helpers/domEvents.js
+++ src/javascripts/helpers/domEvents.js
@@ -4,7 +4,7 @@ import printBoardTitle from '../components/printBoardTitle';
 import showBoards from '../components/showBoards';
 import showPins from '../components/showPins';
 import { getBoards, getSingleBoard } from './data/boardsData';
-import getPins from './data/pinsData';
+import { deletePins, getPins } from './data/pinsData';
 
 const domEvents = (uid) => {
   document.querySelector('body').addEventListener('click', (e) => {
@@ -16,6 +16,11 @@ const domEvents = (uid) => {
     if (e.target.id.includes('board-view')) {
       document.querySelector('#home-title').innerHTML = 'Boards';
       getBoards(uid).then((array) => showBoards(array));
+    }
+    if (e.target.id.includes('delete-btn')) {
+      const pinId = e.target.id.split('--')[1];
+      const boardId = e.target.id.split('--')[2];
+      deletePins(pinId, boardId).then((arr) => showPins(arr));
     }
   });
 };
