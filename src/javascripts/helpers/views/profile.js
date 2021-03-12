03c1f2afccac1e7e154dc85b8c4e893f62b39702
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-03-12 11:48:44 -0600

    fixing bugs

--- src/javascripts/helpers/views/profile.js
+++ src/javascripts/helpers/views/profile.js
@@ -3,8 +3,6 @@ const buildProfile = (userObject) => {
                                                      <h3>${userObject.name}</h3>
                                                      <p> You last logged in at ${userObject.lastSignedIn}. Welcome back!</p>
                                                      `;
-  document.querySelector('#boards-container').innerHTML = `
-                                                          `;
 };
 
 export default buildProfile;
